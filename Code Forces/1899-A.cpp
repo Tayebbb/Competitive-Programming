30a1a8cddec2a42c388a7400150107181e2bb3f0
Mohammed Tayeb <142062420+Tayebbb@users.noreply.github.com>
2024-09-15 23:56:04 +0600

    Update 1899-A.cpp

--- Code Forces/1899-A.cpp
+++ Code Forces/1899-A.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>a;
+        if(a%3==0)
+            cout<<"Second"<<endl;
+        else
+            cout<<"First"<<endl;
+    }
+}
