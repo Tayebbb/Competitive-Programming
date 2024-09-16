e02fc16f22f1e2c1e82a64a354c07e64b10cfaad
Mohammed Tayeb <mohammedtayebibne@gmail.com>
2024-09-17 00:11:33 +0600

    Update

--- LightOJ/ASCII Table.cpp
+++ LightOJ/ASCII Table.cpp
@@ -14,7 +14,8 @@ void solve()
     ll t;
     cin >> t;
     for (int x = 1; x <= t; x++)
-    {   cout<<"Case "<<x<<":\n";
+    {
+        cout << "Case " << x << ":\n";
         char c, c1;
         cin >> c >> c1;
         int diff = abs(c1 - c);
@@ -22,10 +23,11 @@ void solve()
         {
             if (diff % i == 0)
             {
-                cout << i << " " << (94+i-1)/ i << endl;
+                cout << i << " " << (94 + i - 1) / i << endl;
             }
         }
-        cout << diff << " " << (94+diff-1) / diff <<endl<<endl;
+        cout << diff << " " << (94 + diff - 1) / diff << endl
+             << endl;
     }
 }
 int main()
