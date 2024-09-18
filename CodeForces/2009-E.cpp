601ba3accce47a4d5919509ef6b020271eaf05e0
Mohammed Tayeb <mohammedtayebibne@gmail.com>
2024-09-18 21:35:04 +0600

    Update!

---
+++ CodeForces/2009-E.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define kachao                        \
+    ios_base::srightnc_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+using namespace std;
+
+ll solve(ll start, ll end)
+{
+    return end * (end + 1) / 2 - start * (start - 1) / 2;
+}
+
+int main()
+{
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n, k, s = 1, left, right, ans = LONG_LONG_MAX;
+
+        cin >> n >> k;
+
+        ll h = n - 1, lo = 1, mid;
+
+        while (h >= lo)
+        {
+            mid = (h + lo) / 2;
+            left = solve(k, k + mid - 1);
+            right = solve(k + mid, k + n - 1);
+
+            if (left <= right)
+                lo = mid + 1, s = mid;
+            else
+                h = mid - 1;
+        }
+
+        left = solve(k, k + s - 1);
+
+        right = solve(k + s, k + n - 1);
+        s++;
+
+        ans = abs(left - right);
+        left = solve(k, k + s - 1);
+        right = solve(k + s, k + n - 1);
+
+        ans = min(ans, abs(left - right));
+
+        cout << ans << endl;
+    }
+}
--- Template.cpp
+++ Template.cpp
@@ -1,7 +1,4 @@
 #include <bits/stdc++.h>
-#define yes cout << "YES" << endl;
-#define no cout << "NO" << endl;
-#define all(x) x.begin(), x.end()
 #define ll long long int
 #define kachao                        \
     ios_base::sync_with_stdio(false); \
@@ -11,10 +8,35 @@ using namespace std;
 
 void solve()
 {
-    
+    ll n, k;
+    cin >> n >> k;
+    if (n == 2)
+    {
+        cout << 1 << endl;
+        return;
+    }
+
+    ll sum = (n * (2 * k + (n - 1))) / 2;
+    ll mini = LONG_LONG_MAX;
+    ll result;
+   for (ll i = (n+2-1)/2; i < n-(n/3); ++i)
+    {
+        
+        ll h1 = (i * (2 * k + (i - 1))) / 2;
+        ll h2 = sum - h1;
+        result = abs(h1 - h2);   
+        mini = min(result, mini);
+    }
+    cout << mini << endl;
 }
+
 int main()
 {
-    kachao
-    solve();
+    kachao;
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
 }
