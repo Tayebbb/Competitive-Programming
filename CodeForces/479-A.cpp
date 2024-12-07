7388a137716fa1f294c8f04870880d8f2932d527
Mohammed Tayeb <mohammedtayebibne@gmail.com>
2024-12-07 21:37:43 +0600

    Update cf!

--- CodeForces/479-A.cpp
+++ CodeForces/479-A.cpp
@@ -2,29 +2,54 @@
 #define yes cout << "YES" << endl;
 #define no cout << "NO" << endl;
 #define ll long long int
-#define kachao ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define kachao                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
 using namespace std;
-
 void solve()
 {
-   int a,b,c,ans=0;
-   cin>>a>>b>>c;
-   if(a==1 || b==1)
-       ans=b+a;
-   else
-       ans=b*a;
-   if(c==1)
-      ans+=c;
-   else
-      ans*=c;
-   cout<<ans<<endl;
+    int a, b, c, ans = 0;
+    cin >> a >> b >> c;
+    if (a == 1)
+    {
+        ans = a + b;
+        if (c == 1)
+            ans += c;
+        else
+            ans *= c;
+    }
+    else if (b == 1)
+    {
+        if (a > c)
+        {
+            ans = b + c;
+            ans *= a;
+        }
+        else
+        {
+            ans = a + b;
+            if (c == 1)
+                ans += c;
+            else
+                ans *= c;
+        }
+    }
+    else 
+    {
+        if (c == 1)
+            ans = a * (b + c);
+        else
+            ans = a * b * c;
+    }
+    cout << ans << endl;
 }
 
 int main()
 {
     kachao;
-    ll t=1;
-    //cin >> t;
+    ll t = 1;
+    // cin >> t;
     while (t--)
     {
         solve();
