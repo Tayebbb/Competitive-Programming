accec0ea8683766a0ea3e14acd8eac78e7ace18e
Mohammed Tayeb <mohammedtayebibne@gmail.com>
2025-01-13 21:43:47 +0600

    update

---
+++ CodeForces/2055-A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define ll long long int
+#define pb push_back
+#define kachao                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+using namespace std;
+
+void solve()
+{
+    ll n, a, b;
+    cin >> n >> a >> b;
+    ll r = abs(a - b);
+    
+    if (r % 2 == 0)
+        yes 
+    else no
+}
+
+int main()
+{
+    kachao;
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
